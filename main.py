e1e2d1cb3cde9ffa77075cb8482061499b75c9fc
Kalloreinder <matthieu.perrelle@gmail.com>
2025-10-08 16:48:08 +0200

    added a few more user outputs and refined the code. Still needs to be cleaned up. I was trying something...

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import os
+import sys
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
 
 
 def main():
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
+    client = genai.Client(api_key=api_key)
+    try:
+        user_prompt = sys.argv[1]
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=user_prompt)])
+            ]
+        
+        response = client.models.generate_content(
+                model='gemini-2.0-flash-001', contents=messages
+            )
+        
+        try:
+            if sys.argv[2] == "--verbose":
+                print(f"User prompt: {user_prompt}")
+                print(response.text)
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        except IndexError:
+            print(response.text)
+    except IndexError:
+        print("Error: no question was entered")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
