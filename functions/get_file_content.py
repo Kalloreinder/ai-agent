21b8fc3f132785f82fc4caec0aa0c5688394134e
Kalloreinder <matthieu.perrelle@gmail.com>
2025-10-15 17:08:44 +0200

    incomplete get_file_content

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,2 +1,9 @@
+import os
+
 def get_file_content(working_directory, file_path):
-    return 0
+    full_path = os.path.join(working_directory, file_path)
+    abs_work = os.path.abspath(working_directory)
+    abs_full = os.path.abspath(full_path) 
+
+    if abs_work != abs_full:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
