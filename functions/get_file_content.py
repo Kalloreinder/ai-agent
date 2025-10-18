48d83556c0306aac2c3888244da7695b6469433a
Kalloreinder <matthieu.perrelle@gmail.com>
2025-10-18 10:48:54 +0200

    finished get_file_content and ran some tests

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,9 +1,23 @@
 import os
 
+MAX_CHARS = 10_000
+
 def get_file_content(working_directory, file_path):
     full_path = os.path.join(working_directory, file_path)
     abs_work = os.path.abspath(working_directory)
     abs_full = os.path.abspath(full_path) 
 
-    if abs_work != abs_full:
+    if not (abs_work == abs_full or abs_full.startswith(abs_work + os.sep)):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(full_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    try:
+        with open(abs_full, 'r', encoding='utf-8') as f:
+            file_content = f.read(MAX_CHARS)
+            if len(file_content) == MAX_CHARS:
+                file_content += f'[...File "{file_path}" truncated at 10000 characters]'
+        return file_content
+    except ValueError as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,13 +1,16 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-print("Result for current directory:")
-print(get_files_info("calculator", "."))
+print("Result Lorem Ipsum text file:")
+print(get_file_content("calculator", "lorem.txt"))
 
-print("Result for 'pkg' directory:")
-print(get_files_info("calculator", "pkg"))
+print("Results from main.py in calculator folder:")
+print(get_file_content("calculator", "main.py"))
 
-print("Result for '/bin' directory")
-print(get_files_info("calculator", "/bin"))
+print("Results from /bin/cat:")
+print(get_file_content("calculator", "/bin/cat"))
 
-print("Result for '../' directory")
-print(get_files_info("calculator", "../"))
+print("Results from a file that does not exist:")
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
+
+print("Results from calculator.py in calculator folder:")
+print(get_file_content("calculator", "pkg/calculator.py"))
